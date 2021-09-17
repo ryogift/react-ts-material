a84a094d2c210f192cc9a6bf729f9850ac79d3cf
Ryo Takahashi <ryogiftjp@gmail.com>
2021-09-17 23:46:54 +0900

    パッケージの更新

--- src/components/DataTable.tsx
+++ src/components/DataTable.tsx
@@ -6,7 +6,6 @@ import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
-import TablePagination from '@material-ui/core/TablePagination';
 import TableRow from '@material-ui/core/TableRow';
 
 interface Column {
@@ -85,17 +84,8 @@ const useStyles = makeStyles({
 
 const DataTable = () => {
   const classes = useStyles();
-  const [page, setPage] = React.useState(0);
-  const [rowsPerPage, setRowsPerPage] = React.useState(10);
-
-  const handleChangePage = (event: unknown, newPage: number) => {
-    setPage(newPage);
-  };
-
-  const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setRowsPerPage(+event.target.value);
-    setPage(0);
-  };
+  const [page] = React.useState(0);
+  const [rowsPerPage] = React.useState(10);
 
   return (
     <Paper className={classes.root}>
@@ -132,15 +122,6 @@ const DataTable = () => {
           </TableBody>
         </Table>
       </TableContainer>
-      <TablePagination
-        rowsPerPageOptions={[10, 25, 100]}
-        component="div"
-        count={rows.length}
-        rowsPerPage={rowsPerPage}
-        page={page}
-        onChangePage={handleChangePage}
-        onChangeRowsPerPage={handleChangeRowsPerPage}
-      />
     </Paper>
   );
 }
